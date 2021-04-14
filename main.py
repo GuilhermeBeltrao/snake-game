930e1203d04222660dcd6951c6f45c8ebc5b1ca5
GuilhermeBeltrao <guilhermecezar.beltrao@gmail.com>
2021-04-14 15:58:09 -0300

    food

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+
+#   Screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake Game')
+screen.tracer(0)
+
+#   Classes
+snake = Snake()
+food = Food()
+
+#   Movements
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #   Collision Detection
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+screen.exitonclick()
